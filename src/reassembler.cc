1348506d85a3ca751c09ee9dace2ef28c658fe34
Fei Pan <pan-fei@outlook.com>
2023-10-20 00:45:22 +0800

    Added reassembler

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -4,15 +4,55 @@ using namespace std;
 
 void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring, Writer& output )
 {
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
-  (void)output;
+  if (is_last_substring) {
+    last_substring_inserted = true;
+  }
+
+  if (first_index < front) {
+    const uint64_t to_pop = front - first_index;
+    if (to_pop >= data.size()) {
+      return;
+    }
+    data = data.substr(to_pop);
+    first_index = 0;
+  } else {
+    first_index -= front;
+    first_index = std::min(first_index, output.available_capacity());
+  }
+  // Trim bytes that exceed capacity
+  data.resize(std::min(data.size(), output.available_capacity() - first_index));
+  buffer.resize(std::max(buffer.size(), first_index + data.size()));
+
+  for (uint64_t i = 0; i < data.size(); ++i) {
+    auto &[validity, byte] = buffer[i + first_index];
+    byte = data[i];
+    bytes_valid -= validity;
+    validity = true;
+    bytes_valid += validity;
+  }
+
+  uint64_t bytes_ready = 0;
+  while (bytes_ready < buffer.size() && buffer[bytes_ready].first) {
+    ++bytes_ready;
+  }
+  if (bytes_ready) {
+    front += bytes_ready;
+    bytes_valid -= bytes_ready;
+    data.clear();
+    while (bytes_ready--) {
+      data.push_back(buffer.front().second);
+      buffer.pop_front();
+    }
+    output.push(data);
+  }
+
+  if ( last_substring_inserted && buffer.empty()) {
+    output.close();
+  }
 }
 
 uint64_t Reassembler::bytes_pending() const
 {
   // Your code here.
-  return {};
+  return bytes_valid;
 }
