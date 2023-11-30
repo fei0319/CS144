01ccc800741e22041649282f0a650194638cd944
Fei Pan <pan-fei@outlook.com>
2023-11-30 22:55:36 +0800

    Optimized unwrap

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,7 +1,5 @@
 #include "wrapping_integers.hh"
 
-#include <algorithm>
-
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
@@ -14,14 +12,16 @@ uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
   uint32_t const low_bits = raw_value_ - zero_point.raw_value_;
   uint64_t const high_bits = checkpoint >> 32;
 
-  uint64_t const r0 = high_bits << 32 | low_bits;
-  uint64_t const distance = std::min( r0 - checkpoint, checkpoint - r0 );
+  auto distance = []( uint64_t a, uint64_t b ) { return a > b ? a - b : b - a; };
 
-  if ( high_bits != UINT32_MAX && ( r0 + ( 1ULL << 32 ) ) - checkpoint < distance ) {
-    return r0 + ( 1ULL << 32 );
+  uint64_t const r = high_bits << 32 | low_bits;
+  uint64_t const d = distance( checkpoint, r );
+
+  if ( distance( checkpoint, r + ( 1ULL << 32 ) ) < d ) {
+    return r + ( 1ULL << 32 );
   }
-  if ( high_bits != 0 && checkpoint - ( r0 - ( 1ULL << 32 ) ) < distance ) {
-    return r0 - ( 1ULL << 32 );
+  if ( distance( checkpoint, r - ( 1ULL << 32 ) ) < d ) {
+    return r - ( 1ULL << 32 );
   }
-  return r0;
+  return r;
 }
