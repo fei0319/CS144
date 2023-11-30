417a6a5b31f858e337f7957e29715b94b26bfd7d
Fei Pan <pan-fei@outlook.com>
2023-11-30 22:49:27 +0800

    Added wrapping_integers

--- src/wrapping_integers.cc
+++ src/wrapping_integers.cc
@@ -1,19 +1,27 @@
 #include "wrapping_integers.hh"
 
+#include <algorithm>
+
 using namespace std;
 
 Wrap32 Wrap32::wrap( uint64_t n, Wrap32 zero_point )
 {
-  // Your code here.
-  (void)n;
-  (void)zero_point;
-  return Wrap32 { 0 };
+  return zero_point + static_cast<uint32_t>( n );
 }
 
 uint64_t Wrap32::unwrap( Wrap32 zero_point, uint64_t checkpoint ) const
 {
-  // Your code here.
-  (void)zero_point;
-  (void)checkpoint;
-  return {};
+  uint32_t const low_bits = raw_value_ - zero_point.raw_value_;
+  uint64_t const high_bits = checkpoint >> 32;
+
+  uint64_t const r0 = high_bits << 32 | low_bits;
+  uint64_t const distance = std::min( r0 - checkpoint, checkpoint - r0 );
+
+  if ( high_bits != UINT32_MAX && ( r0 + ( 1ULL << 32 ) ) - checkpoint < distance ) {
+    return r0 + ( 1ULL << 32 );
+  }
+  if ( high_bits != 0 && checkpoint - ( r0 - ( 1ULL << 32 ) ) < distance ) {
+    return r0 - ( 1ULL << 32 );
+  }
+  return r0;
 }
